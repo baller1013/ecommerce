b647ab63af590ed39f7c44327c2739d0ac52a0db
Will D <33186828+baller1013@users.noreply.github.com>
2025-01-10 17:53:38 +0000

    Settings.py changes

--- ecom/ecom/ecom/settings.py
+++ ecom/ecom/ecom/settings.py
@@ -134,8 +134,9 @@ STATICFILES_DIRS = ['static/']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
